94f17c46d150a1dfc9caa09762949f2f6b74c502
M. Dicky Haikal Aly <dickyhaa@users.noreply.github.com>
2026-05-27 02:05:15 +0700

    feat: scaffold core administrative and guest views with basic routing and authentication controller

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,49 @@ use Illuminate\Support\Facades\Route;
 
 // Guest Routes
 Route::get('/', function () {
-    return view('guest.home');
+    return view('pages.guest.home');
 })->name('home');
 
 Route::get('/explore', function () {
-    return view('guest.explore');
+    return view('pages.guest.explore');
 })->name('explore');
 
 Route::get('/planner', function () {
-    return view('guest.planner');
+    return view('pages.guest.planner');
 })->name('planner');
 
 Route::get('/blog', function () {
-    return view('guest.blog');
+    return view('pages.guest.blog');
 })->name('blog');
 
 Route::get('/about', function () {
-    return view('guest.about');
+    return view('pages.guest.about');
 })->name('about');
 
 // Admin Protected Routes
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pages.admin.dashboard');
+    })->name('dashboard');
+
+    Route::prefix('admin')->group(function () {
+        // Manajemen Blog Artikel
+        Route::prefix('blog-artikel')->name('admin.blog-artikel.')->group(function () {
+            Route::get('/', function () { return view('pages.admin.blog-artikel.index'); })->name('index');
+            Route::get('/create', function () { return view('pages.admin.blog-artikel.create'); })->name('create');
+            Route::get('/{id}', function () { return view('pages.admin.blog-artikel.show'); })->name('show');
+            Route::get('/{id}/edit', function () { return view('pages.admin.blog-artikel.edit'); })->name('edit');
+        });
+
+        // Kategori Artikel Blog
+        Route::prefix('kategori-blog-artikel')->name('admin.kategori-blog-artikel.')->group(function () {
+            Route::get('/', function () { return view('pages.admin.kategori-blog-artikel.index'); })->name('index');
+            Route::get('/create', function () { return view('pages.admin.kategori-blog-artikel.create'); })->name('create');
+            Route::get('/{id}', function () { return view('pages.admin.kategori-blog-artikel.show'); })->name('show');
+            Route::get('/{id}/edit', function () { return view('pages.admin.kategori-blog-artikel.edit'); })->name('edit');
+        });
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
